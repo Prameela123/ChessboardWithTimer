d107e1ece397127a18ec14212fb04f920bd0a018
Prameela123 <prameakkinapalli@gmail.com>
2023-11-14 16:21:33 +0530

    Add files via upload

---
+++ GridCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  GridCollectionViewCell.swift
+//  TitanProject
+//
+//  Created by Prameela Akkinapalli on 08/09/21.
+//  Copyright © 2021 Prameela Akkinapalli. All rights reserved.
+//
+
+import UIKit
+
+class GridCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+}
